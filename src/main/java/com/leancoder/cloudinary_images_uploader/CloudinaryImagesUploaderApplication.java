c2520dc6266b22f11fd991e8b13b28d2200c3efc
LeandroPalma27 <psychomusicbox@gmail.com>
2023-11-23 00:02:32 -0500

    Inicio de proyecto

---
+++ src/main/java/com/leancoder/cloudinary_images_uploader/CloudinaryImagesUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.leancoder.cloudinary_images_uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudinaryImagesUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudinaryImagesUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leancoder/cloudinary_images_uploader/CloudinaryImagesUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leancoder.cloudinary_images_uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudinaryImagesUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
