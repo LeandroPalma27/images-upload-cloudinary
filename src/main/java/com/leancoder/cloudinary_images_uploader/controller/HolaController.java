90a117012617ab67eee0b2beb5c7d6fa57c3841e
LeandroPalma27 <psychomusicbox@gmail.com>
2023-12-06 12:13:04 -0500

    Avances en el estudion de spring session y jsessionid

--- src/main/java/com/leancoder/cloudinary_images_uploader/controller/HolaController.java
+++ src/main/java/com/leancoder/cloudinary_images_uploader/controller/HolaController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/* CLASE PARA ENTENDER EL PROCESO DE CREACION DE SESSION EN SPRING Y LA ASIGNACION DE UN JSESSIONID */
 @Controller
 public class HolaController {
     
